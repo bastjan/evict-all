ec401897c467e211c314370e9299b7f661e1acc8
Sebastian Widmer <sebastian.widmer@vshn.net>
2023-12-13 16:48:55 +0100

    Implement namespace and pod label selector

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"go.uber.org/multierr"
 	corev1 "k8s.io/api/core/v1"
 	policyv1 "k8s.io/api/policy/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -25,34 +27,71 @@ func init() {
 }
 
 func main() {
-	ns := flag.String("namespace", "default", "namespace to evict pods from")
-	asUser := flag.String("as", "", "impersonate user")
+	var ns, asUser, labelSel, nsLabelSel string
+	flag.StringVar(&ns, "namespace", "default", "namespace to evict pods from")
+	flag.StringVar(&asUser, "as", "", "impersonate user")
+	flag.StringVar(&labelSel, "l", "", "label selector to filter pods by")
+	flag.StringVar(&nsLabelSel, "lns", "", "label selector to filter namespaces by, overrides the namespace flag")
 
 	flag.Parse()
 
 	cfg := config.GetConfigOrDie()
-	cfg.Impersonate.UserName = *asUser
+	cfg.Impersonate.UserName = asUser
 	cl, err := client.New(cfg, client.Options{Scheme: scheme})
 	if err != nil {
 		fmt.Println("failed to create client")
 		os.Exit(1)
 	}
 
-	var pl corev1.PodList
-	if err := cl.List(context.Background(), &pl, client.InNamespace(*ns)); err != nil {
-		fmt.Println("failed to list pods")
+	parsedSel, err := labels.Parse(labelSel)
+	if err != nil {
+		fmt.Println("failed to parse label selector")
 		os.Exit(1)
 	}
 
-	fmt.Printf("evicting %d pods\n", len(pl.Items))
+	var nsl corev1.NamespaceList
+	if nsLabelSel != "" {
+		parsedNsSel, err := labels.Parse(nsLabelSel)
+		if err != nil {
+			fmt.Println("failed to parse label selector")
+			os.Exit(1)
+		}
+		if err := cl.List(context.Background(), &nsl, client.MatchingLabelsSelector{
+			Selector: parsedNsSel,
+		}); err != nil {
+			fmt.Println("failed to list namespaces")
+			os.Exit(1)
+		}
+	} else {
+		nsl.Items = []corev1.Namespace{{ObjectMeta: metav1.ObjectMeta{Name: ns}}}
+	}
+
 	var errs error
-	for _, p := range pl.Items {
-		fmt.Println(p.Name)
-		errs = multierr.Append(errs, cl.SubResource("eviction").Create(context.Background(), &p, &policyv1.Eviction{}))
+	for _, ns := range nsl.Items {
+		errs = multierr.Append(errs, evict(cl, ns.Name, parsedSel))
 	}
 
 	if errs != nil {
 		fmt.Println("failed to evict pods")
 		os.Exit(1)
 	}
+}
+
+func evict(cl client.Client, ns string, sel labels.Selector) error {
+	var pl corev1.PodList
+	if err := cl.List(context.Background(), &pl, client.InNamespace(ns), client.MatchingLabelsSelector{
+		Selector: sel,
+	}); err != nil {
+		fmt.Println("failed to list pods")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Evicting %d pods from %q\n", len(pl.Items), ns)
+	var errs error
+	for _, p := range pl.Items {
+		fmt.Println("Evicting", ns, "/", p.Name)
+		errs = multierr.Append(errs, cl.SubResource("eviction").Create(context.Background(), &p, &policyv1.Eviction{}))
+	}
+
+	return errs
 }
